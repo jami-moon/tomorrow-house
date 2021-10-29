86e062ac175d4d2647e089074f6b9147b9d7a2f3
jami-moon <sherlockyu155@gmail.com>
2021-10-29 17:54:39 +0900

    🚧 Initiate a new carousel: .user-gallery

--- js/Carousels.js
+++ js/Carousels.js
@@ -9,4 +9,22 @@ const productCarousel = tns({
   autoplayButtonOutput: false,
   mouseDrag: true,
   preventScrollOnTouch: true,
-})
+});
+
+const userGallery = tns({
+  container: ".user-gallery .slider-list",
+  gutter: 4,
+  edgePadding: 16,
+  controls: false,
+  loop: false,
+  arrowKeys: true,
+  mouseDrag: true,
+  preventScrollOnTouch: true,
+  responsive: {
+    768: {
+      gutter: 6,
+      edgePadding: 75,
+      controls: true,
+    },
+  },
+});
