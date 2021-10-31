5b730fcfec98773bbb1b098e07d840cf5e88cbd3
jami-moon <sherlockyu155@gmail.com>
2021-10-31 20:32:59 +0900

    🔨 Add active&open toggle function to DOM elements

--- js-guide.md
+++ js-guide.md
@@ -1,19 +1,21 @@
 # 자바스크립트 목록
 
-## 1. 데스크탑 gnb form-input에 foucs 했을 때, search-history에 is-active 클래스 추가
+### ~~1. 데스크탑 gnb form-input에 foucs 했을 때, search-history에 is-active 클래스 추가~~
 
-## 2. 데스트탑 gnb .my-menu에 hover 혹은 클릭 시 .is-active 클래스 추가
+### ~~2. 데스트탑 gnb .my-menu에 클릭 시 .is-active 클래스 추가~~
 
-## 3. 모바일 gnb .is-search 버튼 클릭 시 .search-modal에 is-active 클래스 추가
+### ~~3. 모바일 gnb .is-search 버튼 클릭 시 .search-modal에 is-active 클래스 추가~~
 
-## 4. 모바일 gnb .is-menu 버튼 클릭 시 .sidebar & .overlay 에 is-active 클래스 추가(5번과 연계)
+### ~~4. 모바일 gnb .is-menu 버튼 클릭 시 .sidebar & .overlay 에 is-active 클래스 추가(5번과 연계)~~
 
-## 5. 모바일 .drawer-menu 클릭 시 is-actve & is-open 클래스 추가
+### ~~5. 모바일 .drawer-menu 클릭 시 is-actve & is-open 클래스 추가~~
 
-## 6. 모바일 .product-section-content 안에 버튼 펼치기 클릭 시 .rpodcut-spec에 is-open 클래스 추가
+### ~~6. 모바일 .product-section-content 안에 버튼 펼치기 클릭 시 .prpodcut-spec에 is-open 클래스 추가~~
 
-## 7. 모바일 .product-inquiry 클릭 시 .product-inquiry에 is-open 클래스 추가
+### ~~7. 모바일 .product-inquiry 클릭 시 .product-inquiry에 is-open 클래스 추가~~
 
-## 8. 모바일 .product-shipment 클릭 시 .product-inquiry에 is-open 클래스 추가
+### ~~8. 모바일 .product-shipment 클릭 시 .product-shipment에 is-open 클래스 추가~~
 
-## 9. 모바일 .order-cta에 구매하기 버튼 클릭 시 .order-form-modal에 is-open 클래스 추가
+### ~~9. 모바일 .order-cta에 구매하기 버튼 클릭 시 .order-form-modal에 is-open 클래스 추가~~
+
+### 10. 모달 외부영역 클릭 시에만 모달이 닫힐 수 있도록 설정하기(sidebar, .order-form-modal 등 닫기 버튼이 없는 모달에 적용)
---
+++ js/active&open.js
@@ -0,0 +1,110 @@
+"use strict";
+
+const $searchInput = document.querySelector(".gnb-search .form-input");
+const $searchHistory = document.querySelector(".gnb-search .search-history");
+const $myMenu = document.querySelector(".my-menu");
+const $myMenuBtn = document.querySelector(".my-menu .my-menu-button");
+const $searchIcon = document.querySelector(".gnb-icon-button.is-search");
+const $searchModal = document.querySelector(".search-modal");
+const $searchModalCloseBtn = document.querySelector(".search-modal .close-btn");
+const $menuBtn = document.querySelector(".gnb-icon-button.is-menu");
+const $sidebar = document.querySelector(".sidebar");
+const $overlay = document.querySelector(".overlay");
+const $sidebarMenu = document.querySelectorAll(".sidebar .drawer-menu");
+const $contentBtn = document.querySelector(".product-section-content .open-btn");
+const $productSpec = document.querySelector(".product-section.product-spec");
+const $inquiry = document.querySelector(".product-inquiry");
+const $inquiryHeader = document.querySelector(".product-inquiry .product-section-header");
+const $shipment = document.querySelector(".product-shipment");
+const $shipmentHeader = document.querySelector(".product-shipment .product-section-header");
+const $ctaOrderBtn = document.querySelector(".order-cta .order-btn");
+const $orderFormModal = document.querySelector(".order-form-modal");
+
+const IS_ACTIVE = `is-active`;
+const IS_OPEN = `is-open`;
+
+function activeToggle(elementToBeChanged) {
+  elementToBeChanged.classList.toggle(IS_ACTIVE);
+}
+
+function openToggle(elementToBeChanged) {
+  elementToBeChanged.classList.toggle(IS_OPEN);
+}
+
+$searchInput.addEventListener("focus", () => {
+  activeToggle($searchHistory);
+});
+
+$searchInput.addEventListener("blur", () => {
+  activeToggle($searchHistory);
+});
+
+$myMenuBtn.addEventListener("focus", () => {
+  activeToggle($myMenu);
+});
+
+$myMenuBtn.addEventListener("blur", () => {
+  activeToggle($myMenu);
+});
+
+$searchIcon.addEventListener("focus", () => {
+  activeToggle($searchModal);
+  document.body.style.overflow = "hidden";
+});
+
+$searchModalCloseBtn.addEventListener("click", () => {
+  activeToggle($searchModal);
+  document.body.style.overflow = "visible";
+});
+
+$menuBtn.addEventListener("click", () => {
+  $sidebar.classList.add(IS_ACTIVE);
+  $overlay.classList.add(IS_ACTIVE);
+  document.body.style.overflow = "hidden";
+
+  window.addEventListener("click", removeSidebar);
+});
+
+function removeSidebar(e) {
+  if (e.target === $overlay) {
+    $sidebar.classList.remove(IS_ACTIVE);
+    $overlay.classList.remove(IS_ACTIVE);
+    document.body.style.overflow = "visible";
+    window.removeEventListener("click", removeSidebar);
+  }
+}
+
+$sidebarMenu.forEach((target) => {
+  target.addEventListener("click", () => {
+    target.classList.toggle(IS_ACTIVE);
+    target.classList.toggle(IS_OPEN);
+  });
+});
+
+$contentBtn.addEventListener("click", () => {
+  openToggle($productSpec);
+});
+
+$inquiryHeader.addEventListener("click", () => {
+  $inquiry.classList.add(IS_OPEN);
+});
+
+$shipmentHeader.addEventListener("click", () => {
+  $shipment.classList.add(IS_OPEN);
+});
+
+$ctaOrderBtn.addEventListener("click", () => {
+  $orderFormModal.classList.add(IS_OPEN);
+  $overlay.classList.add(IS_ACTIVE);
+
+  window.addEventListener("click", removeOrderModal);
+});
+
+function removeOrderModal(e) {
+  if (e.target === $overlay) {
+    $orderFormModal.classList.remove(IS_OPEN);
+    $overlay.classList.remove(IS_ACTIVE);
+    document.body.style.overflow = "visible";
+    window.removeEventListener("click", removeSidebar);
+  }
+}
